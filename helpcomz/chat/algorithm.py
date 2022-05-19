8fcd7d9da8a25ee38044b41b323ed6477f7d90f7
joon <sh05158@flysher.net>
2022-05-20 07:49:03 +0900

    add path

--- helpcomz/chat/algorithm.py
+++ helpcomz/chat/algorithm.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*- 
 import sys
 import os
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
 import pandas as pd
 from .csvManager import CsvManager
